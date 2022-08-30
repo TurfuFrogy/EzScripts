9cd7a24bea1170823522864e16c8c787efcf60d7
Frogy <107172174+TurfuFrogy@users.noreply.github.com>
2022-08-30 15:14:51 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,56 @@
+repeat
+    task.wait()
+until 
+    game:IsLoaded()
 
+-- if writefile then
+--     writefile("EzScripts.win-key", "")
+-- end
+
+local library = loadstring(game:HttpGet(("https://raw.githubusercontent.com/TurfuFrogy/EzScripts/main/ui.lua")))()
+
+library:MakeNotification({
+    Name = "EzScripts.win",
+    Content = "Thanks for using our script!",
+    Image = "rbxassetid://10715004387",
+    Time = 5
+})
+
+local window = library:MakeWindow({
+    Name = "EzScripts.win - Loader",
+    HidePremium = true, 
+    SaveConfig = false, 
+    ConfigFolder = "EzScripts/",
+    IntroEnabled = true,
+    IntroText = "EzScripts.win",
+    IntroIcon = "rbxassetid://10715004387",
+    Icon = "rbxassetid://10715004387",
+})
+
+local homeTab = window:MakeTab({
+    Name = "Home",
+    Icon = "rbxassetid://10715004387",
+    PremiumOnly = false
+})
+
+homeTab:AddButton({
+    Name = "Load EzScripts.win!",
+    Callback = function()
+        library:MakeNotification({
+            Name = "EzScripts.win",
+            Content = "Loading. This can take some times.",
+            Image = "rbxassetid://10715004387",
+            Time = 5
+        })
+
+        -- if readfile then
+        --     getgenv().Key = readfile("EzScripts.win-key")
+        -- else
+        getgenv().Key = ""
+        -- end
+
+        loadstring(game:HttpGet("https://scripts.luawl.com/12261/EzScripts.lua"))()
+    end
+})
+
+library:Init()
